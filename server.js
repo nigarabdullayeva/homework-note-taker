5bddad85ec0bc1e8c046fb388b5f07cec4b9c0b6
Nigar Abdullayeva <nigar_abdullaeva@hotmail.com>
2020-07-24 17:12:15 -0500

    adding routes and port listener

--- server.js
+++ server.js
@@ -9,6 +9,20 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+//routes 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/notes",(req, res) => {
+    res.sendFile(path.join(__dirname, "public", "notes.html"));
+
+})
 
 
+//starts server to begin listening
+app.listen(PORT, function() {
+    console.log(`App listening on PORT: ${PORT}`);
+  })
